0883857d2efc9476b6d2600dc541fa672d783446
Simon Prickett <simon@moduscreate.com>
2016-09-12 22:57:13 -0700

    Replaces Windows make batch file with one for Mac OS.

--- make.bat
+++
@@ -1,4 +0,0 @@
-path=%PATH%;C:\gbadev\tools\devkitadv\bin
-
-gcc  -o tron.elf tron.c
-objcopy -O binary tron.elf tron.gba
---
+++ make.sh
@@ -0,0 +1,4 @@
+arm-none-eabi-gcc -mthumb -mthumb-interwork -c tron.c
+arm-none-eabi-gcc -specs=gba.specs -mthumb -mthumb-interwork tron.o -o tron.elf
+arm-none-eabi-objcopy -O binary tron.elf tron.gba
+gbafix tron.gba
